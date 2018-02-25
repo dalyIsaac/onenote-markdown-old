8a7bdbb3baf97c156e6ede06d8444a2109f2bfa3
DalyIsaac <isaac.daly@outlook.com>
2018-02-25 17:46:27 +1300

    Working routing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
+import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
+    router: routerReducer,
     users: userReducer
 });
