9dbef5ae7cc3f0e90c224db9e70cdb0ea74723e9
DalyIsaac <isaac.daly@outlook.com>
2018-02-28 22:11:40 +1300

    Implemented spinner

--- src/components/notebookPicker.js
+++ src/components/notebookPicker.js
@@ -1,40 +1,58 @@
-import * as React from 'react';
-import { Modal } from 'office-ui-fabric-react/lib/Modal';
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+import * as React from "react";
+import { Modal } from "office-ui-fabric-react/lib/Modal";
+import { DefaultButton } from "office-ui-fabric-react/lib/Button";
+import { Spinner, SpinnerSize } from "office-ui-fabric-react/lib/Spinner";
+import "./notebookPicker.css";
 
 export class NotebookPickerComponent extends React.Component {
-
   constructor(props) {
     super(props);
     this._showModal = this._showModal.bind(this);
     this._closeModal = this._closeModal.bind(this);
     this.state = {
-      showModal: false
+      showModal: false,
+      spinnerVisible: true
     };
+  }
+
+  componentWillReceiveProps() {
+    this.setState({ spinnerVisible: false });
   }
 
   render() {
     return (
       <div>
         <DefaultButton
-          description='Opens the Sample Modal'
-          onClick={ this._showModal }
-          text='Open Modal'
+          description="Opens the Sample Modal"
+          onClick={this._showModal}
+          text="Open Modal"
         />
         <Modal
-          isOpen={ this.state.showModal }
-          onDismiss={ this._closeModal }
-          isBlocking={ false }
-          containerClassName='ms-modalExample-container'
+          isOpen={this.state.showModal}
+          onDismiss={this._closeModal}
+          isBlocking={false}
+          containerClassName="ms-modalExample-container"
         >
-          <h1>Hello, world!</h1>
+          <div className="parent">
+            {this.state.spinnerVisible && (
+              <Spinner
+                className="spinner"
+                size={SpinnerSize.large}
+                label="Hang on, I'm asking around for your notebooks..."
+                ariaLive="assertive"
+              />
+            )}
+          </div>
         </Modal>
       </div>
     );
   }
 
   _showModal() {
-    this.setState({ showModal: true });
+    this.setState({
+      showModal: true,
+      spinnerVisible: true
+    });
     this.props.getAllNotebooks();
   }
 
--- src/containers/notebookPicker.js
+++ src/containers/notebookPicker.js
@@ -1,13 +1,15 @@
-import { NotebookPickerComponent } from '../components/notebookPicker';
-import { connect } from 'react-redux';
-import { getNotebooks } from '../actions';
+import { NotebookPickerComponent } from "../components/notebookPicker";
+import { connect } from "react-redux";
+import { getNotebooks } from "../actions";
 
-const mapStateToProps = () => ({
-    // nothing here yet
+const mapStateToProps = state => ({
+  notebooks: state.allNotebooks
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    getAllNotebooks: () => dispatch(getNotebooks.getAllNotebooks())
+const mapDispatchToProps = dispatch => ({
+  getAllNotebooks: () => dispatch(getNotebooks.getAllNotebooks())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(NotebookPickerComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(
+  NotebookPickerComponent
+);
