a2a2dc304182aad1cdb3c842ae1619573c7a940d
DalyIsaac <isaac.daly@outlook.com>
2018-03-02 15:39:59 +1300

    Updated types in authentication

--- src/sagas/authentication.js
+++ src/sagas/authentication.js
@@ -1,52 +1,54 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put } from "redux-saga/effects";
 
-import { UserData } from './../types';
-import { authentication }from '../actions';
-import { graphScopes } from '../constants';
-import { blobUrl, getToken, currentToken } from './index';
-import { betaUrl } from '../constants';
+import { UserData } from "./../types";
+import { authentication } from "../actions";
+import { graphScopes } from "../constants";
+import { blobUrl, getToken, currentToken } from "./index";
+import { betaUrl } from "../constants";
 
-import axios from 'axios';
+import axios from "axios";
 
 export function* authenticate(action) {
-    const userList = yield call([action.app, action.app.getAllUsers]);
-    if (userList.length > 0) {
-        const userDataList = userList.map(x => new UserData(x));
-        yield put(authentication.newUserList(userDataList));
-        for (let i = 0; i < userDataList.length; i++) {
-            const user = userDataList[i];
-            yield put(authentication.getPhoto(user.msal));
-        }
+  const userList = yield call([action.app, action.app.getAllUsers]);
+  if (userList.length > 0) {
+    const userDataList = userList.map(x => new UserData(x));
+    yield put(authentication.newUserList(userDataList));
+    for (let i = 0; i < userDataList.length; i++) {
+      const user = userDataList[i];
+      yield put(authentication.getPhoto(user));
     }
+  }
 }
 
 export function* signIn(action) {
-    yield call([action.app, action.app.loginRedirect], graphScopes);
-    // no need for a put because the app redirects
+  yield call([action.app, action.app.loginRedirect], graphScopes);
+  // no need for a put because the app redirects
 }
 
 export function* signOut(action) {
-    yield call([action.app, action.app.logout]);
-    // no need for a put because the app redirects
+  yield call([action.app, action.app.logout]);
+  // no need for a put because the app redirects
 }
 
 export function* getPhoto(action) {
-    yield call(getToken, action.app, action.user);
-    if (currentToken !== '') {
-        try {
-            const result = yield call(axios, {
-                method: 'get',
-                responseType: 'blob',
-                url: betaUrl + 'me/photo/$value',
-                headers: { Authorization: `Bearer ${currentToken}` }
-            });
-            const photo = result.data && blobUrl(result.data);
-            const newUser = new UserData(action.user, photo);
-            yield put(authentication.updateUser(newUser))
-        } catch (error) {
-            console.error(`Getting photo failed for ${action.user.displayableId}: ${error}`);
-        }
-    } else {
-        console.error('No token');
+  yield call(getToken, action.app, action.user);
+  if (currentToken !== "") {
+    try {
+      const result = yield call(axios, {
+        method: "get",
+        responseType: "blob",
+        url: betaUrl + "me/photo/$value",
+        headers: { Authorization: `Bearer ${currentToken}` }
+      });
+      const photo = result.data && blobUrl(result.data);
+      const newUser = new UserData(action.user.msal, photo);
+      yield put(authentication.updateUser(newUser));
+    } catch (error) {
+      console.error(
+        `Getting photo failed for ${action.user.msal.displayableId}: ${error}`
+      );
     }
+  } else {
+    console.error("No token");
+  }
 }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,44 +1,64 @@
-import { takeEvery, takeLatest, call, put } from 'redux-saga/effects';
+import { takeEvery, takeLatest, call, put } from "redux-saga/effects";
 
-import { graphScopes } from '../constants';
-import { UserData, AUTHENTICATE, SIGN_IN, SIGN_OUT, GET_PHOTO, GET_ALL_NOTEBOOKS } from './../types';
-import { authentication }from '../actions';
+import { graphScopes } from "../constants";
+import {
+  UserData,
+  AUTHENTICATE,
+  SIGN_IN,
+  SIGN_OUT,
+  GET_PHOTO,
+  GET_ALL_NOTEBOOKS
+} from "./../types";
+import { authentication } from "../actions";
 
-import { authenticate, signIn, signOut, getPhoto } from './authentication';
-import { getAllNotebooks } from './notebooks';
+import { authenticate, signIn, signOut, getPhoto } from "./authentication";
+import { getAllNotebooks } from "./notebooks";
 
 export default function* rootSaga() {
-    yield takeLatest(AUTHENTICATE, authenticate);
-    yield takeLatest(SIGN_IN, signIn);
-    yield takeLatest(SIGN_OUT, signOut);
-    yield takeEvery(GET_PHOTO, getPhoto);
-    yield takeLatest(GET_ALL_NOTEBOOKS, getAllNotebooks);
+  yield takeLatest(AUTHENTICATE, authenticate);
+  yield takeLatest(SIGN_IN, signIn);
+  yield takeLatest(SIGN_OUT, signOut);
+  yield takeEvery(GET_PHOTO, getPhoto);
+  yield takeLatest(GET_ALL_NOTEBOOKS, getAllNotebooks);
 }
 
 const urls = new WeakMap();
 
 // code courtesy of https://www.bignerdranch.com/blog/dont-over-react/
 export const blobUrl = blob => {
-    if (urls.has(blob)) {
-      return urls.get(blob)
-    } else {
-      let url = URL.createObjectURL(blob)
-      urls.set(blob, url)
-      return url
-    }
+  if (urls.has(blob)) {
+    return urls.get(blob);
+  } else {
+    let url = URL.createObjectURL(blob);
+    urls.set(blob, url);
+    return url;
   }
+};
 
 // Storing the token as a variable outside, instead of yielding is to avoid interfering with redux-saga.
 // It should be thought of as an instance variable
-export let currentToken = '';
+export let currentToken = "";
 
+/**
+ *
+ *
+ * @export
+ * @param {UserAgentApplication} app
+ * @param {UserData} user
+ */
 export function* getToken(app, user) {
   try {
-    currentToken = yield call([app, app.acquireTokenSilent], graphScopes, null, user);
+    currentToken = yield call(
+      [app, app.acquireTokenSilent],
+      graphScopes,
+      null,
+      user.msal
+    );
   } catch (error) {
-    currentToken = '';
-    console.error(`Token error for ${user}: ${error} `);
-    const newUser = UserData(user.msal, '', error)
+    currentToken = "";
+    console.error(`Token error for ${user.msal.displayableId}`);
+    console.error(error);
+    const newUser = new UserData(user.msal, "", error);
     yield put(authentication.updateUser(newUser));
   }
 }
