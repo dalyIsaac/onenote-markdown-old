9310bedbd515965969e1a87d201366a42d3c9100
dalyIsaac <isaac.daly@outlook.com>
2018-07-22 01:05:11 +1200

    Updated imports for settings

--- src/components/settings/settings.test.tsx
+++ src/components/settings/settings.test.tsx
@@ -1,13 +1,13 @@
 import * as enzyme from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
 import * as React from "react";
-import { SettingsComponent } from "./settings";
+import Settings from "./settings";
 
 enzyme.configure({ adapter: new Adapter() });
 
 describe("Components: Settings", () => {
   test("Renders", () => {
-    const wrapper = enzyme.mount(<SettingsComponent />);
+    const wrapper = enzyme.mount(<Settings />);
     expect(wrapper.find("p").exists()).toBe(true);
     expect(wrapper.text()).toBe("Welcome to settings");
   });
