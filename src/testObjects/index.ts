9ae02878267cdfbe524281398f0ec3f3234e20ed
dalyIsaac <isaac.daly@outlook.com>
2018-07-22 16:37:37 +1200

    Extracted testObjects' onenote

--- src/testObjects/index.ts
+++ src/testObjects/index.ts
@@ -15,23 +15,13 @@ export * from "./notebookOrder";
 export * from "./openedNotebooks";
 export * from "./graphSectionInstances";
 export * from "./sections";
-import { IStateOneNote } from "../reducers";
-import { notebook, notebook1 } from "./notebooks";
-import { page, page1 } from "./pages";
-import { sectionGroup, sectionGroup1 } from "./sectionGroups";
-import { section, section1 } from "./sections";
+export * from "./onenote";
+import { notebook1 } from "./notebooks";
+import { page1 } from "./pages";
+import { sectionGroup1 } from "./sectionGroups";
+import { section1 } from "./sections";
 
 export const parentSelfUser = "jane.doe@email.com";
-
-export const onenote: IStateOneNote = {};
-onenote[notebook.id] = notebook;
-onenote[notebook1.id] = notebook1;
-onenote[sectionGroup.id] = sectionGroup;
-onenote[sectionGroup1.id] = sectionGroup1;
-onenote[section.id] = section;
-onenote[section1.id] = section1;
-onenote[page.id] = page;
-onenote[page1.id] = page1;
 
 export const selectedNav: string[] = [
   notebook1.id,
