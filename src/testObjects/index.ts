7326eb2e2bbc45f32706de9cc9dc6af6086f9b65
dalyIsaac <isaac.daly@outlook.com>
2018-07-22 16:30:04 +1200

    Extracted testObjects' sections

--- src/testObjects/index.ts
+++ src/testObjects/index.ts
@@ -14,29 +14,12 @@ export * from "./allNotebooks";
 export * from "./notebookOrder";
 export * from "./openedNotebooks";
 export * from "./graphSectionInstances";
+export * from "./sections";
 import { IStateOneNote } from "../reducers";
-import { Section } from "../types/Section";
-import { graphPageInstance, graphPageInstance1 } from "./graphPageInstances";
-import {
-  graphSectionInstance,
-  graphSectionInstance1
-} from "./graphSectionInstances";
 import { notebook, notebook1 } from "./notebooks";
 import { page, page1 } from "./pages";
 import { sectionGroup, sectionGroup1 } from "./sectionGroups";
-import { userId, userId1 } from "./users";
-
-export const section = new Section(
-  graphSectionInstance,
-  [[graphPageInstance, graphPageInstance1]],
-  userId
-);
-
-export const section1 = new Section(
-  graphSectionInstance1,
-  [[graphPageInstance1]],
-  userId1
-);
+import { section, section1 } from "./sections";
 
 export const parentSelfUser = "jane.doe@email.com";
 
